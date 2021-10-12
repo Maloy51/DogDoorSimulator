c4111aa898ff4a51515e6f2c028b101c31128aa4
Maloy51 <malkin-sergej@yandex.ru>
2021-10-12 21:14:09 +0300

    Изменение варианта открытия двери

--- OneGuitar/DoorsForDog/DogDoorSimulator.cs
+++ OneGuitar/DoorsForDog/DogDoorSimulator.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace DoorsForDog
 {
@@ -8,14 +9,26 @@ namespace DoorsForDog
         {
             DogDoor door = new DogDoor();
             Remote remote = new Remote(door);
-            Console.WriteLine("Фидо лает, чтобы выйти на улицу...");
+            //Console.WriteLine("Фидо лает, чтобы выйти на улицу...");
+            Console.WriteLine();
             remote.pressButton();
             Console.WriteLine("\nФидо ушел на улицу...");
-            remote.pressButton();
-            Console.WriteLine("\nФидо все готово...");
-            remote.pressButton();
+            Console.WriteLine("\nФидо всё готово...");
+            //remote.pressButton();
+            try
+            {
+                Thread.Sleep(10000);
+            }
+            catch (ThreadInterruptedException e){}
+            Console.WriteLine("...но он застрял снаружи!");
+            Console.WriteLine("\nФидо начинает лаять...");
+            Console.WriteLine("... Джина хватает пульт дистанционного управления.");
             Console.WriteLine("\nФидо вернулся в дом...");
             remote.pressButton();
+            //remote.pressButton();
+
+            Console.WriteLine("Нажмите ENTER, для окончании программы..");
+            Console.ReadLine();
         }
     }
 }
